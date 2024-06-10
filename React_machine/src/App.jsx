c26e3ed83b7fce0641c6ac582c9d7ddc85fbc967
Pankaj Kumar <pankajs2167@gmal.com>
2024-06-10 14:17:38 +0530

    completed responsive navbar

--- React_machine/src/App.jsx
+++ React_machine/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import InfiniteScroll from './Component/InfiniteScroll'
+import NavbarComp from './Component/NavbarComponent/NavbarComp'
 
 export default function App() {
   return (
     <>
-      <InfiniteScroll/>
+      {/* <InfiniteScroll/> */}
+      <NavbarComp/>
     </>
   )
 }
